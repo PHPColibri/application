7ad1ccad1f4f91f77fa562f6a1e96dfd0efc56f7
alek13 <alek13.me@gmail.com>
2014-11-23 00:50:17 +0300

    add required configs

---
+++ application/configs/routing.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	'rewrite' => array(
+		// for example:
+		/*array(
+			'pattern'		=> '/\/nissan\/+(.*)\/(.*)/',
+			'replacement'	=> 'articles/show/nissan_$1_$2'
+		)/*,
+		array(
+			'pattern'		=> '/\/nissan\/+(.*)\/(.*)/',
+			'replacement'	=> 'articles/show/nissan_$1_$2'
+		)*/
+	),
+	
+	// не используется: 'routes' => array(...),
+);
